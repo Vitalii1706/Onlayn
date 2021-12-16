99037e458a751cf592c4c3793845ca4031211af1
Vitalii1706 <kitral.v.o@gmail.com>
2021-12-16 11:52:59 +0200

    1

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const listElem = document.querySelector('.list');
 
 const tasks = [
-  { id: 0, text: 'Buy milk', done: false },
-  { id: 1, text: 'Pick up Tom from airport', done: false },
-  { id: 2, text: 'Visit party', done: false },
-  { id: 3, text: 'Visit doctor', done: true },
-  { id: 4, text: 'Buy meat', done: true },
+  { id: 1, text: 'Buy milk', done: false },
+  { id: 2, text: 'Pick up Tom from airport', done: false },
+  { id: 3, text: 'Visit party', done: false },
+  { id: 4, text: 'Visit doctor', done: true },
+  { id: 5, text: 'Buy meat', done: true },
 ];
 
 const renderTasks = (tasksList) => {
@@ -13,13 +13,13 @@ const renderTasks = (tasksList) => {
 
   const tasksElems = tasksList
     .sort((a, b) => a.done - b.done)
-    .map(({ text, done }, index) => {
+    .map(({ id, text, done }) => {
       const listItemElem = document.createElement('li');
       listItemElem.classList.add('list__item');
       const checkbox = document.createElement('input');
 
       checkbox.setAttribute('type', 'checkbox');
-      checkbox.setAttribute('data-id', index);
+      checkbox.setAttribute('data-id', id);
       checkbox.checked = done;
 
       checkbox.classList.add('list__item-checkbox');
@@ -51,15 +51,13 @@ renderTasks(tasks);
 // 3. re-render
 
 function undateTaskHandler(event) {
-  console.log(event.target.classList.contains('list__item-checkbox'));
-  console.log(event.target.getAttribute('type'));
-
   if (!event.target.classList.contains('list__item-checkbox')) {
     return;
   }
-
-  const id = event.target.dataset.id;
-
+  const task = tasks.find((el) => el.id === Number(event.target.dataset.id));
+  task.done = event.target.checked;
+  //task.done = !task.done;
+  renderTasks(tasks);
   // TODO
 }
 
@@ -75,3 +73,25 @@ const res = listElem.addEventListener('click', undateTaskHandler);
 // 2. render (відмалювали сторінку)
 // 3. update data, don`t update DOM(!!!!!) (оновлення данних, не працюється з ДОМ )
 // 4. re-render (знову відмалювали сторінку)   ///
+
+function createTaskHandler() {
+  const inputElem = document.querySelector('.task-input');
+  const str = inputElem.value;
+
+  if (str === '') {
+    return;
+  }
+
+  inputElem.value = '';
+
+  tasks.push({ text: str, done: false, id: tasks.length + 1 });
+  renderTasks(tasks);
+}
+const createBtnElem = document.querySelector('.create-task-btn');
+createBtnElem.addEventListener('click', createTaskHandler);
+
+// algo
+// 1. get task text
+// 2. create task
+// 3. update task
+// 4. re-render
